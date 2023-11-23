3b21e7083ae110329f8b050e14ba8328381d2bcd
Andres-XL <amovilla1103@gmail.com>
2023-11-22 21:57:46 -0500

    question crud

--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -3,14 +3,6 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const categoryRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: publicProcedure
     .input(
       z.object({
--- src/server/api/routers/question.ts
+++ src/server/api/routers/question.ts
@@ -3,14 +3,6 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const questionRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: publicProcedure
     .input(
       z.object({
@@ -18,7 +10,7 @@ export const questionRouter = createTRPCRouter({
         body: z.string().min(1),
         answer: z.string().min(1),
         options: z.array(z.string().min(1)).min(1),
-        category: z.string().min(1),
+        flashcard: z.string().min(1),
       }),
     )
     .mutation(async ({ ctx, input }) => {
@@ -28,14 +20,78 @@ export const questionRouter = createTRPCRouter({
           body: input.body,
           answer: input.answer,
           options: input.options,
-          category: input.category,
+          flashcard: input.flashcard,
         },
       });
     }),
 
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.question.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
+  getAll: publicProcedure
+    .input(
+      z.object({
+        flashcard: z.string().optional(),
+      }),
+    )
+    .query(({ ctx, input }) => {
+      return ctx.db.question.findMany({
+        where: {
+          flashcard: input.flashcard,
+        },
+        orderBy: { createdAt: "desc" },
+      });
+    }),
+
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.string().min(1),
+        title: z.string().min(1),
+        body: z.string().min(1),
+        options: z.array(z.string().min(1)).min(1),
+        answer: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.question.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          title: input.title,
+          body: input.body,
+          options: input.options,
+          answer: input.answer,
+        },
+      });
+    }),
+
+  delete: publicProcedure
+    .input(
+      z.object({
+        id: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.question.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  findOne: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .query(({ ctx, input }) => {
+      if (!input.id) return undefined;
+
+      return ctx.db.category.findFirst({
+        where: {
+          id: input.id,
+        },
+        orderBy: { createdAt: "desc" },
+      });
+    }),
 });
